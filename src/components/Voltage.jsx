0967b66f660547e0ebf4034025abbddf05487c5a
maximmim <93773088+maximmim@users.noreply.github.com>
2024-06-10 10:25:10 +0300

    Add files via upload

--- src/components/Voltage.jsx
+++ src/components/Voltage.jsx
@@ -7,17 +7,42 @@ function random(number1, number2) {
 const Voltage = () => {
   const [voltage, setVoltage] = useState(parseFloat(localStorage.vol));
   const [lastClickTime, setLastClickTime] = useState(0);
+  const [edin, setEdin] = useState("V");
+
 
   useEffect(() => {
-    localStorage.vol = voltage;
-    if (voltage >= 12) {
-      const r = random(0, 255);
-      const g = random(0, 255);
-      const b = random(0, 255);
+    localStorage.vol = voltage;      
+    const r = random(0, 255);
+    const g = random(0, 255);
+    const b = random(0, 255);
 
+    if (voltage >= 12) {
+      document.getElementsByClassName("Voltage")[0].classList.add("awd");
+      document.getElementsByClassName("Voltage")[0].style.border = `2px solid rgb(${r},${g},${b})`;
+    }
+    if (voltage >= 100) {
       document.getElementsByClassName("Voltage")[0].classList.add("awd");
       document.getElementsByClassName("Voltage")[0].style.border = `4px solid rgb(${r},${g},${b})`;
     }
+    if (voltage >= 220) {
+      document.getElementsByClassName("Voltage")[0].classList.add("awd");
+      document.getElementsByClassName("Voltage")[0].style.border = `5px solid rgb(${r},${g},${b})`;
+    }
+    if (voltage >= 500) {
+      document.getElementsByClassName("Voltage")[0].classList.add("awd");
+      document.getElementsByClassName("Voltage")[0].style.border = `6px solid rgb(${r},${g},${b})`;
+    }
+    if (voltage >= 1000) {
+      document.getElementsByClassName("Voltage")[0].classList.add("awd");
+      document.getElementsByClassName("Voltage")[0].style.border = `4px solid rgba(${r},${g},${b} 0.5)`;
+      setEdin("kV")
+    }
+    if (voltage >= 10000) {
+      document.getElementsByClassName("Voltage")[0].classList.add("awd");
+      document.getElementsByClassName("Voltage")[0].style.border = `4px solid rgb(${r},${g},${b} 0.5)`;
+      setEdin("MV")
+    }
+
   }, [voltage]);
 
   function wd() {
@@ -32,10 +57,8 @@ const Voltage = () => {
       setLastClickTime(currentTime);
     }
     else {
-      localStorage.vol = 0
-        setVoltage(0);
-      alert("НАДТО ВЕЛИКИЙ ВОЛЬТАЖЖЖЖЖЖ !!!!")
 
+      document.getElementById("neon-image").classList.add("active");
 
     }
   }
@@ -44,7 +67,7 @@ const Voltage = () => {
     <>
       <div className="Voltage">
         <div className="Voltage_state">
-          <p>{voltage.toFixed(1)}v</p>
+          <p>{voltage.toFixed(1)}<span>{edin}</span></p>
         </div>
 
         <img
